cdf7fda6204070693947b5022528de7296bee80f
Demetr <dmitri.martynov@gmail.com>
2023-12-13 18:24:11 +0100

    move stripes from base class to derived Tiger and add spots to Elephanrt

--- NewTricksAnalogue/Classes.py
+++ NewTricksAnalogue/Classes.py
@@ -2,10 +2,9 @@
 class Animal:
     type = "Animal"
 
-    def __init__(self, color="Grey", size="Small", stripes=False):
+    def __init__(self, color="Grey", size="Small"):
         self.color = color
         self.size = size
-        self.stripes = stripes
 
     # declaring __str__() method
     def __str__(self) -> str:
@@ -13,10 +12,7 @@ class Animal:
 
     # declaring __repr__() method
     def __repr__(self):
-        o = self
-        return_str = (f'The creature is {o.size} {o.type}, it is {o.color} and ' +
-                      ("has stripes" if o.stripes else "has no stripes"))
-        return return_str
+        return f'The creature is {self.size} {self.type}, it is {self.color}'
 
 
 class Tiger(Animal):
@@ -24,7 +20,8 @@ class Tiger(Animal):
     stripes_color = "Brown"
 
     def __init__(self, color="Orange", size="Medium", stripes=True, stripes_color="Black"):
-        super().__init__(color, size, stripes)
+        super().__init__(color, size)
+        self.stripes = stripes
         self.stripes_color = stripes_color
 
     # declaring __repr__() method
@@ -34,37 +31,45 @@ class Tiger(Animal):
                       (f"has {o.stripes_color} stripes" if o.stripes else "has no stripes"))
         return return_str
 
-#TODO: What to do with stripes versus spots?
-# class Elephant(Animal):
-#     type = "Elephant"
-#     spots_color = "Brown"
-#
-#     def __init__(self, color="Orange", size="Medium", spots=True, spots_color="Pink"):
-#         super().__init__(color, size, spots)
-#         self.spots_color = spots_color
-#
-#     # declaring __repr__() method
-#     def __repr__(self):
-#         o = self
-#         return_str = (f'The creature is {o.size} {o.type}, it is {o.color} and ' +
-#                       (f"has {o.spots_color} stripes" if o.stripes else "has no stripes"))
-#         return return_str
+
+class Elephant(Animal):
+    type = "Elephant"
+    spots = False
+
+    def __init__(self, color="Greyish Black", size="Large", spots=False, spots_color="Pink"):
+        super().__init__(color, size)
+        self.spots_color = spots_color
+        self.spots = spots
+
+    # declaring __repr__() method
+    def __repr__(self):
+        o = self
+        return_str = (f'The creature is {o.size} {o.type}, it is {o.color} and ' +
+                      (f"has {o.spots_color} spots" if o.spots else "has no spotes"))
+        return return_str
+
 
 def main():
-    obj = Animal("Brown", "Large", False)
+    obj = Animal("Brown", "Large")
     print(obj)
     print(str(obj))
     print(repr(obj))
-
+    print("---------------------")
     obj = Tiger()
     print(obj)
     print(str(obj))
     print(repr(obj))
-
+    print("---------------------")
     obj = Tiger("Golden Orange", "Medium", True, "Brown")
     print(obj)
     print(str(obj))
     print(repr(obj))
+    print("---------------------")
+    obj = Elephant("Grey", "Large", True, "Pink")
+    print(obj)
+    print(str(obj))
+    print(repr(obj))
+
 
 if __name__ == '__main__':
     main()
